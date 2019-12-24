5f313470a18809902ed976dc4fdbd564618e7884
DaniilSafin <44696193+DaniilSafin@users.noreply.github.com>
2019-12-24 13:13:34 +0500

    Update forum.php

--- forum.php
+++ forum.php
@@ -1,9 +1,9 @@
 <html>
 <head>
 <title>Мини-чатъ!</title>
-<frameset rows="30%,70%">
-<frame name="addmsg" src="addmsg.php">
-<frame name="showmsg" src="showmsg.php">
-</frameset>
 </head>
+<frameset rows="30%,70%">
+<frame src="addmsg.php">
+<frame src="showmsg.php">
+</frameset>
 </html>
